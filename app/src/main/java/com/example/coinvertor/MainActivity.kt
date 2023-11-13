ee6f33307197042fd3186cdf75e0e38325690aac
obscure-star <iibeanusiwork@gmail.com>
2023-11-12 21:06:27 -0800

    remove binding

--- app/src/main/java/com/example/coinvertor/MainActivity.kt
+++ app/src/main/java/com/example/coinvertor/MainActivity.kt
@@ -5,12 +5,12 @@ import android.os.Bundle
 import com.example.coinvertor.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
+    // private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        //binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(R.layout.activity_main)
 
         //setupClickListeners()
     }
