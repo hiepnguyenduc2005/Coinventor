d3ed31f1c45c5fb704123b064c5b0724ae62bd62
Dezzysdream <dantesnava@gmail.com>
2023-11-13 20:51:44 -0500

    Initial recycler view

---
+++ app/src/main/java/com/example/coinvertor/CountryAdapter.kt
@@ -0,0 +1,37 @@
+package com.example.coinvertor
+
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.signature.ObjectKey
+
+class CountryAdapter(private val countryList: MutableList<MainActivity.Country>): RecyclerView.Adapter<CountryAdapter.ViewHolder>(){
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val countryImage: ImageView  = view.findViewById(R.id.country_image)
+
+    }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.country_item, parent, false)
+
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val country = countryList[position]
+        Log.d("country","$country")
+        Glide.with(holder.itemView)
+            .load(country.flagUrl) // Assuming you have a property like "flagUrl" in your Country class
+            .into(holder.countryImage)
+    }
+
+    override fun getItemCount() = countryList.size
+
+}
+
+
+
--- app/src/main/java/com/example/coinvertor/MainActivity.kt
+++ app/src/main/java/com/example/coinvertor/MainActivity.kt
@@ -2,18 +2,96 @@ package com.example.coinvertor
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.coinvertor.databinding.ActivityMainBinding
+import android.util.Log
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.codepath.asynchttpclient.AsyncHttpClient
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler
+import okhttp3.Headers
+
 
 class MainActivity : AppCompatActivity() {
-    // private lateinit var binding: ActivityMainBinding
+    private lateinit var countryList: MutableList<Country>
+    private lateinit var rvCountry: RecyclerView
+    private lateinit var adapter: CountryAdapter
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        //binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(R.layout.activity_main)
+    data class Country(
+        val name: String,
+        val capital: String,
+        val population: Int,
+        val flagUrl: String,
+        // Add other properties as needed
+    )
 
-        //setupClickListeners()
+        // private late init var binding: ActivityMainBinding
+
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            //binding = ActivityMainBinding.inflate(layoutInflater)
+            setContentView(R.layout.activity_main)
+
+            rvCountry = findViewById(R.id.country_list)
+            countryList = mutableListOf()
+
+            adapter = CountryAdapter(countryList)
+
+            rvCountry.adapter = adapter
+            rvCountry.layoutManager = LinearLayoutManager(this)
+
+            getCountryImageURL()
+
+        }
+    private fun getCountryImageURL() {
+        val client = AsyncHttpClient()
+
+
+        client["https://restcountries.com/v3.1/currency/cop", object : JsonHttpResponseHandler() {
+            override fun onSuccess(statusCode: Int, headers: Headers, json: JsonHttpResponseHandler.JSON) {
+                val countryArray = json.jsonArray
+
+                for (i in 0 until countryArray.length()) {
+                    val countryObject = countryArray.getJSONObject(i)
+
+                    val name = countryObject.getJSONObject("name").getString("common")
+                    val capital = countryObject.getJSONArray("capital").getString(0)
+                    val population = countryObject.getInt("population")
+                    val flagUrl = countryObject.getJSONObject("flags").getString("png")
+
+                    countryList.add(Country(name, capital, population, flagUrl))
+
+                }
+                Log.d("Country", "Number of countries: ${countryList.size}")
+
+                // Check if the adapter is null or empty before updating it
+                if (::adapter.isInitialized) {
+
+                    Log.d("Country", "Adapter updated")
+                } else {
+                    Log.e("Country", "Adapter not initialized")
+                }
+
+                Log.d("Country", "Response successful")
+
+                val adapter = CountryAdapter(countryList)
+                rvCountry.adapter = adapter
+                rvCountry.layoutManager = LinearLayoutManager(this@MainActivity)
+                Log.d("Country", "response successful")
+            }
+
+            override fun onFailure(
+                statusCode: Int,
+                headers: Headers?,
+                errorResponse: String,
+                throwable: Throwable?
+            ) {
+                Log.d("Country Error", errorResponse)
+            }
+        }]
+
     }
+
+    }
+
 
     /*private fun setupClickListeners(){
         compute.setOnClickListener { computeButtonClicked() }
@@ -32,4 +110,3 @@ class MainActivity : AppCompatActivity() {
         super.onDestroy()
         runningCountJob?.cancel()
     }*/
-}
